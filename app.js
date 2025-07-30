640d40ad41d5c25c4e8696e681a63a5d828af55e
dartagnam <dartagnam.ferreira@gmail.com>
2025-07-30 11:46:00 -0300

    Primeiras Funçoes

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Declaração de variáveis
+let amigos = [];
+
+
+//Funçoes
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+
+    if (nome == 0) {
+        alert('Por favor, insira um nome.');
+    } else {
+        amigos.push(nome);
+        console.log(amigos);
+        limparCampo();
+        listarAmigos();
+    }
+}
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
+
+function listarAmigos() {
+    let lista = document.getElementById('listaAmigos');
+
+    //limpar a lista antes de adicionar itens
+    lista.innerHTML = '';
+
+    //Listando amigos e criando um <li>
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
+}
+
